f4c78741260bcd0d83130799d8b7c149a963e3e5
ahmetkca <ahmet.karapinar@ontariotechu.net>
2021-03-29 20:54:28 -0400

    added multiplication uni test

--- test_calculator.py
+++ test_calculator.py
@@ -8,3 +8,6 @@ class TestCalculator:
 
     def test_subtraction(self):
         assert 5 == Calculator.subtract(10, 5)
+
+    def test_multiplication(self):
+        assert 36 == Calculator.multiply(6, 6)
